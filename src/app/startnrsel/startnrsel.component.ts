75e87367b056628ca1aebc336a69f9e66096fe47
MathysDev <michael@mmhu.ch>
2023-11-12 17:49:11 +0100

    Fokus setzen bei Startnr Eingabe
    Signed-off-by: MathysDev <michael@mmhu.ch>

--- src/app/startnrsel/startnrsel.component.ts
+++ src/app/startnrsel/startnrsel.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UntypedFormGroup, UntypedFormBuilder } from '@angular/forms';
-import {  OnDestroy,  VERSION } from '@angular/core';
+import {  OnDestroy,  VERSION, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { fromEvent, merge, of, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { OfflineService } from '../shared/offline.service';
+import {    ViewChild, ElementRef } from '@angular/core';
 
 
 
@@ -16,20 +17,32 @@ import { OfflineService } from '../shared/offline.service';
 })
 
 
-export class StartnrselComponent implements OnInit {
+export class StartnrselComponent implements OnInit, AfterViewInit  {
 	form!: UntypedFormGroup;
 	startnr :string = "";
 	Startnummerfehlt: boolean = false;
 	notfound!: any;
 	networkStatus: boolean = true;
+	@ViewChild("mystartnr") myStartnrField: ElementRef | undefined;
+	
+  constructor(private router: Router , private fb: UntypedFormBuilder ,public of: OfflineService,private route: ActivatedRoute ,private el: ElementRef){ }
+  
+	ngAfterViewInit() {
+		console.log('loaded startnrsel');
+		if (this.myStartnrField) {
+			this.myStartnrField.nativeElement.focus();
+		}
+	}
 
-  constructor(private router: Router , private fb: UntypedFormBuilder ,public of: OfflineService,private route: ActivatedRoute ){ }
-  
-  
+	// ...
+
  
   ngOnInit(): void {
 	this.route.paramMap.subscribe(params => { 
-	this.notfound = params.get('notfound');});
+	this.notfound = params.get('notfound');
+
+	
+});
 	this.form = this.fb.group({ startnr: [null] })  
 	this.of.checkNetworkStatus()
 	
