7f525f8e7fa42694b31a1a97cc8a93af1578cbde
MathysDev <67581434+MathysDev@users.noreply.github.com>
2024-06-23 14:46:32 +0200

    ng update (#19)
    * ng update
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    * try with other path
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    * ng patch fire
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    * ng update
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    * correct app-module providers
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    * fix vulnerabilities
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    * try with npm install
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    * update ws
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    * add firebase-tools
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    * update ng to 18
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    * use new node
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    * add vite
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    * add force
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    * install
    
    Signed-off-by: MathysDev <michael@mmhu.ch>
    
    ---------
    
    Signed-off-by: MathysDev <michael@mmhu.ch>

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const dir = "src/environments";
+const dir = "src/environments/";
 const file = "environment.ts";
 const prodFile = "environment.prod.ts";
 const cypressFile = "cypress.env.json";
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,42 +17,29 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { StartnrselComponent } from './startnrsel/startnrsel.component';
 import { LoginComponent } from './login/login.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import 'init';
 
 
 
 
-@NgModule({
-
-  declarations: [
-    AppComponent,
-    TeilnehmerComponent,
-    LaufzettelComponent,
-    LaufzettelListComponent,
-    StartnrselComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideFirestore(() => {
-      const  firestore = getFirestore();
-      
-      return  firestore;
-}),
-
-    
-  ],
-  providers: [ ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        TeilnehmerComponent,
+        LaufzettelComponent,
+        LaufzettelListComponent,
+        StartnrselComponent,
+        LoginComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        RouterModule,
+        FormsModule,
+        AppRoutingModule,
+        ReactiveFormsModule], providers: [provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideFirestore(() => {
+            const firestore = getFirestore();
+            return firestore;
+        }), provideHttpClient(withInterceptorsFromDi()),] })
 export class AppModule { 
 
 }
